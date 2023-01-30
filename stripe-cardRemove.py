cd7114422cd699d7c1441cf607db41d09090100f
John Malagon <71419964+johnnymalagon30@users.noreply.github.com>
2023-01-30 14:55:38 -0600

    Update and rename card-remove.py to stripe-cardRemove.py

--- stripe-cardRemove.py
+++ stripe-cardRemove.py
@@ -1,8 +1,14 @@
+#This script takes all the associated payment methods attached to a customer account and wipes them from the account. This is necessary because QA has several test a
+#accounts with hundreds of payment methods and they eventually reach the limit that Stripe allows.
+
+#Import relevant libraries
+
 import stripe
 import csv
 import requests
 import json
 
+# Color schema for easier reading of terminal output
 class color:
     PURPLE = '\033[95m'
     CYAN = '\033[96m'
@@ -15,16 +21,19 @@ class color:
     UNDERLINE = '\033[4m'
     END = '\033[0m'
 
+#Place the appropriate dev or prod key here
 stripe.api_key = "xxxx"
 
+#Opens .csv file where error information will be placed during runtime.
 file_name = 'errorLog.csv'
 with open(file_name, 'w', newline='') as csvfile:
     membershipwriter = csv.writer(csvfile, delimiter=',')
     membershipwriter.writerow(['method'])
 
+#Define the recursive function
 def real_extract():
     get = stripe.Customer.list_payment_methods(
-      "cus_M0IIHc2fdgJhXc",
+      "xxxxxxx", #place the unique customer ID of the QA account here===
       type="card",
       limit=500
     )
@@ -49,7 +58,8 @@ def real_extract():
                 print(color.RED + 'Failed to cancel method {}'.format(paymentId) + color.END)
                 with open(file_name, 'a', newline='') as csvfile:
                     membershipwriter = csv.writer(csvfile, delimiter=',')
-                    membershipwriter.writerow([paymentId])
+                    membershipwriter.writerow([paymentId]) #If an error is encountered, the payment ID will be placed in the error log file that was
+                                                           #created earlier in the script
 
         real_extract()
 
